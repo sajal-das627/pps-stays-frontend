c0ba6e7fc30c4d39f62b3f52ec87b1ff07d60043
sajaldascutx <sajaldas@cutx.org>
2025-06-13 17:11:08 +0530

    include header file

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../components/Header'; // 👈 New import
 import { Add, Remove } from '@mui/icons-material'; // Add this at the top
 import {
   AppBar,
@@ -50,60 +51,7 @@ const LandingPage = () => {
   return (
     <Box>
       {/* AppBar */}
-      <HideOnScroll>
-        <AppBar position="sticky" color="transparent" elevation={0}>
-          <Toolbar
-            sx={{
-              justifyContent: 'space-between',
-              px: { xs: 2, md: 6 },
-              py: 1,
-              backgroundColor: '#ffffff',
-            }}
-          >
-            <Box component="img" src="/images/credit-union-of-texas-logo.svg" alt="CUTX" sx={{ height: 48 }} />
-
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
-              <Box
-                component="img"
-                src="/images/us-flag.svg"
-                alt="US Flag"
-                sx={{ height: 20 }}
-              />
-              <Box
-                component="a"
-                href="tel:+18005551234"
-                sx={{ display: 'flex', alignItems: 'center' }}
-              >
-                <Box
-                  component="img"
-                  src="/images/call-icon.svg"
-                  alt="Call"
-                  sx={{ height: 20, cursor: 'pointer' }}
-                />
-              </Box>
-
-              <Button
-                variant="outlined"
-                sx={{
-                  borderRadius: 4,
-                  color: '#0050C8',
-                  borderColor: '#0050C8',
-                  fontWeight: 600,
-                  px: 3,
-                  textTransform: 'none',
-                  '&:hover': {
-                    borderColor: '#003e99',
-                    backgroundColor: '#f2f6ff',
-                  },
-                }}
-                onClick={() => navigate('/login')}
-              >
-                Login / Signup
-              </Button>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
+      <Header />
 
       {/* Hero Section */}
       <Box
